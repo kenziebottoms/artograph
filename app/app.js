1c75dd09c2dd7deba7942fc5b7106c21effba701
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-26 11:17:07 -0500

    add app.js and sample.env, package json scripts

---
+++ app/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
