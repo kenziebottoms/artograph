9eaf6387aa9b5ad4f8bb54141f3fdf7c1bce57cf
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-02 14:33:11 -0500

    pins showing up on map sometimes???

--- client/ctrl/artistMapC.js
+++ client/ctrl/artistMapC.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('artograph').controller('ArtistMapCtrl', function ($scope, GOOGLE, GeolocationFactory) {
+angular.module('artograph').controller('ArtistMapCtrl', function ($scope, GOOGLE, GeolocationFactory, ArtistFactory) {
   GeolocationFactory.geolocate()
     .then(coords => {
       $scope.drawMap({ lat: coords.latitude, lng: coords.longitude });
@@ -8,9 +8,24 @@ angular.module('artograph').controller('ArtistMapCtrl', function ($scope, GOOGLE
     .catch(err => console.log(err));
 
   $scope.drawMap = ({ lat, lng }) => {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 8,
-      center: { lat, lng }
-    });
+    ArtistFactory.getAll()
+      .then(artists => {
+        let map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: { lat, lng }
+        });
+        let markers = artists.map(a => {
+          let marker = new google.maps.Marker({
+            position: { lat: parseFloat(a.lat), lng: parseFloat(a.lng) }
+          });
+          marker.addListener('click', () => {
+            let info = new google.maps.InfoWindow();
+            info.setContent(`<h3>${a.name}</h3>`);
+            info.open(map, marker);
+          });
+          return marker;
+        });
+        let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'img/m' });
+      })
   };
 });
