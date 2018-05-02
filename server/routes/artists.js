1fbe7d09d5824f335bec3b38e90a0918bc1a0f11
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-02 16:19:33 -0500

    nearby sorts artists by distance from point

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -3,15 +3,40 @@
 const { Router } = require('express');
 const router = Router();
 const _ = require('lodash');
+const { Op } = require('sequelize');
 
 router.get('/', (req, res, next) => {
   const { Artist } = req.app.get('models');
   // returns all artists alphabetized by last name
   Artist.findAll()
     .then(artists => {
-      artists = _.sortBy(artists, [a => a.name.split(' ').reverse().join(' ')]);
-      res.status(200).json(artists);
+      res.status(200).json(sortAlphabetically(artists));
     })
+    .catch(err => next(err));
+});
+
+router.get('/nearby', (req, res, next) => {
+  const { Artist } = req.app.get('models');
+  let { lat, lng, allowance } = req.query;
+  Artist.findAll({
+    where: {
+      // latitude is within allowance lat/long points of given latitude
+      lat: {
+        [Op.and]: {
+          [Op.gt]: parseFloat(lat) - parseFloat(allowance),
+          [Op.lt]: parseFloat(lat) + parseFloat(allowance)
+        }
+      },
+      lng: {
+        // longitude is within allowance lat/long points of given longitude
+        [Op.and]: {
+          [Op.gt]: parseFloat(lng) - parseFloat(allowance),
+          [Op.lt]: parseFloat(lng) + parseFloat(allowance)
+        }
+      }
+    }
+  })
+    .then(artists => res.status(200).json(sortByDistance(artists, { lat, lng })))
     .catch(err => next(err));
 });
 
@@ -23,5 +48,13 @@ router.get('/:id', (req, res, next) => {
     })
     .catch(err => next(err));
 });
+
+const sortByDistance = (artists, { lat, lng }) => {
+  return _.sortBy(artists, a => Math.sqrt(Math.pow(lat - a.lat, 2) + Math.pow(lng - a.lng, 2)));
+}
+const sortAlphabetically = (artists) => {
+
+  return _.sortBy(artists, [a => a.name.split(' ').reverse().join(' ')]);
+};
 
 module.exports = router;
