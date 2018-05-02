91f50d1a31b15bb0fdaeb4b8ce1069b23bdec423
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-02 11:19:01 -0500

    alphabetized artists by last name

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -2,11 +2,14 @@
 
 const { Router } = require('express');
 const router = Router();
+const _ = require('lodash');
 
 router.get('/', (req, res, next) => {
   const { Artist } = req.app.get('models');
+  // returns all artists alphabetized by last name
   Artist.findAll()
     .then(artists => {
+      artists = _.sortBy(artists, [a => a.name.split(' ').reverse()[0]]);
       res.status(200).json(artists);
     })
     .catch(err => next(err));
