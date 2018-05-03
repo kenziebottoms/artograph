fc1102e5c92eb3098da90d38266b0bc2a7f2c959
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-03 11:51:44 -0500

    reorganized artistMapC

--- client/ctrl/artistListC.js
+++ client/ctrl/artistListC.js
@@ -6,10 +6,10 @@ angular.module('artograph').controller('ArtistListCtrl', function ($rootScope, $
   let geo = { lat: null, lng: null };
 
   ArtistFactory.getAll()
-  .then(artists => {
-    $scope.artists = artists;
-  })
-  .catch(err => console.log(err));
+    .then(artists => {
+      $scope.artists = artists;
+    })
+    .catch(err => console.log(err));
   // try to geolocate
   GeolocationFactory.geolocate()
     .then(({ lat, lng }) => {
@@ -31,6 +31,13 @@ angular.module('artograph').controller('ArtistListCtrl', function ($rootScope, $
     } else {
       $scope.highlight = null;
     }
+  });
+  $rootScope.$on('resortByDistance', (event, { lat, lng }) => {
+    ArtistFactory.getAllByDistance({ lat, lng })
+      .then(artists => {
+        $scope.artists = artists;
+      })
+      .catch(err => console.log(err));
   });
 
 });
--- client/ctrl/artistMapC.js
+++ client/ctrl/artistMapC.js
@@ -2,61 +2,75 @@
 
 angular.module('artograph').controller('ArtistMapCtrl', function ($rootScope, $scope, GOOGLE, GeolocationFactory, ArtistFactory) {
 
+  // $scope variables
+  const star = {
+    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+    anchor: new google.maps.Point(130, 150),
+    fillColor: 'red',
+    fillOpacity: 0.8,
+    scale: 0.13,
+    strokeColor: 'brown',
+    strokeWeight: 1
+  };
+  $scope.map =  new google.maps.Map(document.getElementById('map'));
+  $scope.markerCluster = null;
+  // one info window that moves to whatever point is clicked
+  $scope.info = new google.maps.InfoWindow();
+  $scope.activeMarker = new google.maps.Marker({ icon: star, map: $scope.map });
+
+  // onload
   ArtistFactory.getAll()
     .then(artists => {
-      $scope.drawMap(artists, { lat: 0, lng: 0 });
+      drawMap(artists, { lat: 0, lng: 0 });
     })
-    .catch(err => console.log('ArtistFactory.getAll error', err));
+    .catch(err => console.log(err));
 
-  let map, markerCluster, markers;
-  $scope.drawMap = (artists, { lat, lng }) => {
-    $scope.artists = artists;
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: (lat == 0 && lng == 0) ? 2 : 8,
-      center: { lat, lng }
-    });
-    // one info window that moves to whatever point is clicked
-    let info = new google.maps.InfoWindow();
+  const getMarkers = artists => {
     // extract lat/long from artists
-    markers = artists.map(a => {
+    let markers = artists.map(a => {
       let marker = new google.maps.Marker({
         position: { lat: parseFloat(a.lat), lng: parseFloat(a.lng) }
       });
       // add info window listener to each artist point
       marker.addListener('click', () => {
-        info.setContent(`<h5 data-id='${a.id}'>${a.name}</h5>`);
-        info.open(map, marker);
+        $scope.info.setContent(`<h5 data-id='${a.id}'>${a.name}</h5>`);
+        $scope.info.open($scope.map, marker);
       });
       return marker;
     });
+    return markers;
+  };
+
+  const drawMap = (artists, { lat, lng }) => {
+    // map artist points
+    $scope.artists = artists;
+    // zoom in if there is a non-default epicenter
+    $scope.map.setZoom((lat == 0 && lng == 0) ? 2 : 8);
+    // recenter on given epicenter
+    $scope.map.setCenter({ lat, lng })
+
+    let markers = getMarkers(artists);
+    
     // clusterize the whole thing
-    let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'img/m' });
+    $scope.markerCluster = new MarkerClusterer($scope.map, markers, { imagePath: 'img/m' });
+    google.maps.event.addListener($scope.map, 'click', activateMapListeners);
+  };
 
-    // you are here star
-    let star = {
-      path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-      anchor: new google.maps.Point(130, 150),
-      fillColor: 'red',
-      fillOpacity: 0.8,
-      scale: 0.13,
-      strokeColor: 'brown',
-      strokeWeight: 1
-    };
-    $scope.activeMarker = new google.maps.Marker({ icon: star });
-
-    // listen for non-point clicks
-    google.maps.event.addListener(map, 'click', (event) => {
+  // listen for non-point clicks
+  const activateMapListeners = event => {
+      // hide highlighted artist
       selectArtist(null);
-      info.close();
+      // close all (one) info windows
+      $scope.info.close();
+      // location of click
       let point = {
         lat: event.latLng.lat(),
         lng: event.latLng.lng()
       };
 
       // move existing active marker
-      $scope.activeMarker.setMap(map);
       $scope.activeMarker.setPosition(point);
-    });
+      $rootScope.$broadcast('resortByDistance', point);
   };
 
   const selectArtist = id => {
@@ -68,9 +82,11 @@ angular.module('artograph').controller('ArtistMapCtrl', function ($rootScope, $s
       selectArtist(event.target.dataset.id);
     }
   };
+
   $rootScope.$on('recenterMap', (event, { lat, lng }) => {
-    map.setCenter({ lat, lng });
-    map.setZoom(8);
-    let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'img/m' });
+    $scope.map.setCenter({ lat, lng });
+    $scope.map.setZoom(8);
+    let markers = getMarkers($scope.artists);
+    $scope.markerCluster = new MarkerClusterer($scope.map, markers, { imagePath: 'img/m' });
   });
 });
