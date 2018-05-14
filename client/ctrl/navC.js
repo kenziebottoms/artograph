e9a27d9d02d8b35ad90748aa88d8dfcf7e9afb6f
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-14 09:52:34 -0500

    getUser() fn and listener for  'user change'

--- client/ctrl/navC.js
+++ client/ctrl/navC.js
@@ -1,5 +1,22 @@
 'use strict';
 
-angular.module('artograph').controller('NavCtrl', function($rootScope, $scope) {
-  
+angular.module('artograph').controller('NavCtrl', function ($rootScope, $scope, UserFactory) {
+
+  const refreshUser = () => {
+    UserFactory.getActiveUser()
+      .then(user => $scope.user = user)
+      .catch(err => {
+        if (err.status != 401) console.log(err);
+      });
+  };
+
+  // IMMEDIATE ACTION
+
+  refreshUser();
+
+  // LISTENERS
+
+  $rootScope.$on('user change', event => {
+    refreshUser();
+  });
 });
