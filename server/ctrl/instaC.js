b50c3cff68a7f964772b99f95de501c0812a0cd6
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-01-09 17:42:01 -0600

    return a 500 when insta hits a nonexistent user handle

--- server/ctrl/instaC.js
+++ server/ctrl/instaC.js
@@ -1,22 +1,30 @@
-'use strict';
+"use strict";
 
-const request = require('request');
-const INSTA = 'https://www.instagram.com';
-const _ = require('lodash');
+const request = require("request");
+const INSTA = "https://www.instagram.com";
+const _ = require("lodash");
 
 // get all page data from React object
 const getSharedData = user => {
   return new Promise((resolve, reject) => {
     request(`${INSTA}/${user}`, (err, response, body) => {
-      if (err) return reject({
-        status: 500,
-        message: "Instagram's being a bitch."
-      });
+      if (err)
+        return reject({
+          status: 500,
+          message: "Instagram's being a bitch."
+        });
       // find the `_sharedData` variable in `body`
-      let script = body.split('window._sharedData')[1];
+      let script = body.split("window._sharedData")[1];
       // grab the next JSON object starting with `{` until it ends with `;`
-      script = script.slice(script.indexOf('{')).split(';')[0];
-      resolve(JSON.parse(script));
+      script = script.slice(script.indexOf("{")).split(";")[0];
+      try {
+        resolve(JSON.parse(script));
+      } catch (err) {
+        return reject({
+          status: 500,
+          message: "Instagram's being a bitch."
+        });
+      }
     });
   });
 };
@@ -38,8 +46,10 @@ module.exports.getInfo = user => {
           username
         } = user;
 
-        let feed = data.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges;
-        feed = feed.slice(0,4).map(f => {
+        let feed =
+          data.entry_data.ProfilePage[0].graphql.user
+            .edge_owner_to_timeline_media.edges;
+        feed = feed.slice(0, 4).map(f => {
           return {
             thumb: f.node.thumbnail_src,
             shortcode: f.node.shortcode
@@ -60,16 +70,18 @@ module.exports.getInfo = user => {
         resolve(response);
       })
       .catch(err => reject(err));
-    });
-}
+  });
+};
 
 // scrape 4 most recent image URLs and post links
 module.exports.getUserPosts = uid => {
   return new Promise((resolve, reject) => {
     getSharedData(uid)
       .then(data => {
-        let feed = data.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges;
-        feed = feed.slice(0,4);
+        let feed =
+          data.entry_data.ProfilePage[0].graphql.user
+            .edge_owner_to_timeline_media.edges;
+        feed = feed.slice(0, 4);
         feed = feed.map(f => {
           return {
             thumb: f.node.thumbnail_src,
