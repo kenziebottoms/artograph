12857f3c191b220951fafbc5c40e1b1441be3e3c
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-05-07 19:49:46 -0500

    remove second pw on login

--- client/ctrl/loginC.js
+++ client/ctrl/loginC.js
@@ -20,18 +20,14 @@ angular.module('artograph').controller('LoginCtrl', function ($rootScope, $scope
 
   // post form info to /api/login
   $scope.login = () => {
-    let { password, username, confirmation } = $scope;
-    if (confirmation == password) {
-      $scope.error = null;
-      UserFactory.login({ username, password })
-        .then(response => {
-          $scope.error = null;
-          $rootScope.$broadcast('user change');
-          $location.path('/');
-        })
-        .catch(err => $scope.error = err.data.message);
-    } else {
-      $scope.error = 'Your passwords don\'t match.';
-    }
+    let { password, username } = $scope;
+    $scope.error = null;
+    UserFactory.login({ username, password })
+      .then(response => {
+        $scope.error = null;
+        $rootScope.$broadcast('user change');
+        $location.path('/');
+      })
+      .catch(err => $scope.error = err.data.message);
   };
 });
